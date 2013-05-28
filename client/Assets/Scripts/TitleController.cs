1f9cf80767541220ab9472b1aa787f5274559663
Hiroaki Shirai <hshirai@aiming-inc.com>
2013-05-28 21:35:10 +0900

    Add title button

--- client/Assets/Scripts/TitleController.cs
+++ client/Assets/Scripts/TitleController.cs
@@ -12,4 +12,14 @@ public class TitleController : MonoBehaviour
     {
 	
 	}
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 100, 60), "gumonji");
+
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Start"))
+        {
+            Application.LoadLevel("World");
+        }
+    }
 }
