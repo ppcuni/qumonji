a12d6c4ae8907257e0dfa504a83ea81485447f2e
Hiroaki Shirai <hshirai@aiming-inc.com>
2013-05-30 23:53:26 +0900

    Add camera control script

---
+++ client/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float distance = 20.0f;
+
+    [SerializeField]
+    private float dy = 1.0f;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    void Awake()
+    {
+        UpdateRotation();
+	}
+
+	void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            UpdateRotation();
+        }
+
+        UpdatePosition();
+	}
+
+    private void UpdateRotation()
+    {
+        x += Input.GetAxis("Mouse X") * 100.0f * 0.1f;
+        y -= Input.GetAxis("Mouse Y") * 100.0f * 0.1f;
+
+        transform.rotation = Quaternion.Euler(y, x, 0);
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position = transform.localRotation * new Vector3(0.0f, dy, -distance) + target.position;
+    }
+}
