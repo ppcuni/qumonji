2edb4b738d8f7889b7cdfeb6633a2621164555a3
Hiroaki Shirai <hshirai@aiming-inc.com>
2013-05-31 00:19:37 +0900

    Add character controller

---
+++ client/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float force = 5.0f;
+
+    public bool Jump { get; private set; }
+
+    void Awake()
+    {
+        Jump = true;
+    }
+
+	void Update()
+    {
+        if (Jump) return;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            AddForce(Vector3.left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            AddForce(Vector3.forward);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            AddForce(Vector3.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            AddForce(Vector3.back);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump = true;
+            AddForce(Vector3.up);
+        }
+	}
+
+    private void AddForce(Vector3 vector)
+    {
+        transform.rigidbody.AddForce(vector * force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Jump = false;
+    }
+}
